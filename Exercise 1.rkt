edcbdd0253f639f4d98451d646b47893b3b5e429
Yonatan Ratner <yonaratner@gmail.com>
2022-11-12 18:18:47 +0200

    added .bak to gitignore + tests for 1.a

--- Exercise 1.rkt
+++ Exercise 1.rkt
@@ -7,9 +7,12 @@
   (cond [(null? lst) null]
         [else (append (first lst) (open-list (rest lst)))]))
 
-; 1.a - tests:
-(test (open-list null) => null)
-(test (open-list '((1 2 3) (2 3 3 4) (9 2 -1) (233 11 90))) => '(1 2 3 2 3 3 4 9 2 -1 233 11 90))
+; 1.a - 5 tests:
+(test (open-list null) => null) ;sanity check
+(test (open-list '()) => null) ;sanity check 2
+(test (open-list '((1 2 3) (2 3 3 4) (9 2 -1) (233 11 90))) => '(1 2 3 2 3 3 4 9 2 -1 233 11 90)) ; given test
+(test (open-list '((1 2 3 4) (5) (6 7) (8 9 10 11 12 13 14 15))) => '(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15))
+(test (open-list '((1) ( ) (5) (6 7))) => '(1 5 6 7)) ; can handle 'null' in the form of an empty list - ( )
 
 #| 1.b - min&max |#
 
